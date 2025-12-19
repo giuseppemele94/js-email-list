aec610f119db363fbd81e30f766ea8f755ae6aa2
Giuseppe Mele <giuseppemele9410@gmail.com>
2025-12-19 14:50:58 +0100

    feat : prova stampa su console di una mail

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ Abbellire con CSS o Bootstrap
 Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le altre)
  */
 
+//SETUP INIZIALE 
+const myOutput = document.getElementById("test"); 
+const endpoint  = "https://flynn.boolean.careers/exercises/api/random/mail";
+
+axios.get(endpoint)
+.then(response => {
+
+    //codice da eseguire in caso di successo 
+    const result = response.data ; 
+    console.log(result);
+})
+.catch(error => {
+    //codice da eseguire in caso di errore
+    console.error(error.message)
+})
+.finally(()=> {
+    //codice da eseguire sempre 
+    console.log("ok"); 
+})
