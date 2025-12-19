1ea23e6e954b0379fa1ef68c9a5f71811cfd299d
Giuseppe Mele <giuseppemele9410@gmail.com>
2025-12-19 15:45:21 +0100

    feat : aggiunta funzione randomEmail e button per rigenerare email

--- script.js
+++ script.js
@@ -10,25 +10,45 @@ Inserire un bottone che al click faccia il fetch altre 10 mail (sostituendo le a
  */
 
 //SETUP INIZIALE 
-const myOutput = document.getElementById("test"); 
-const endpoint  = "https://flynn.boolean.careers/exercises/api/random/mail";
 
-for (let i = 0; i < 10; ++i ){
-axios.get(endpoint)
-.then(response => {
+// lista <ul> dove verranno stampate le email
+const emailList = document.getElementById("email-list");
+// endpoint API
+const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail";
+// bottone per rigenerare le email
+const generateEmail = document.querySelector("button");
 
-    //codice da eseguire in caso di successo 
-    const email = response.data ; 
-   
-    //aggiungo un <li> alla lista 
-    myOutput.innerHTML += `<li>${email.response}</li>` ; 
-})
-.catch(error => {
-    //codice da eseguire in caso di errore
-    console.error(error.message)
-})
-.finally(()=> {
-    //codice da eseguire sempre 
-    console.log("ok"); 
-});
+//prima generazione automatica
+randomEmail();
+
+//al click del bottone rigenero le email
+generateEmail.addEventListener("click", randomEmail);
+
+
+//Funzione principale 
+function randomEmail() {
+
+    //svuoto la lista prima di inserire le nuove email 
+    emailList.innerHTML = "";
+
+    //genero 10 email casuali
+    for (let i = 0; i < 10; ++i) {
+        axios.get(endpoint)
+            .then(response => {
+
+                //estraggo l'email dalla risposta API 
+                const email = response.data;
+
+                //aggiungo l'email come <li> alla lista 
+                emailList.innerHTML += `<li>${email.response}</li>`;
+            })
+            .catch(error => {
+                //codice da eseguire in caso di errore
+                console.error("Errore nel recupero dell'email",error); 
+            })
+            .finally(() => {
+                //codice da eseguire sempre 
+                console.log("Operazione andata a buon fine");
+            });
+    }
 }
